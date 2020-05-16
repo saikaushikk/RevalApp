4557adea554d430d9c715086025a042452a9b0d0
sanjanasukumar <sanjana121399@gmail.com>
2020-05-16 17:06:51 +0530

    Added File Picker

--- app/src/main/java/com/example/revalapp/ApplicationFragment.java
+++ app/src/main/java/com/example/revalapp/ApplicationFragment.java
@@ -1,9 +1,16 @@
 package com.example.revalapp;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -14,6 +21,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -25,6 +33,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.HashMap;
 import java.util.Map;
 
+import static android.app.Activity.RESULT_OK;
 import static androidx.constraintlayout.widget.Constraints.TAG;
 
 
@@ -55,7 +64,8 @@ public class ApplicationFragment extends Fragment {
     TextView Tgrade;
     // Access a Cloud Firestore instance from your Activity
     FirebaseFirestore db;
-
+    Uri pdfUri;
+    TextView notif;
 
     public ApplicationFragment() {
         // Required empty public constructor
@@ -102,6 +112,27 @@ public class ApplicationFragment extends Fragment {
         Tcode = view.findViewById(R.id.edit_code);
         Tgrade = view.findViewById(R.id.edit_grade);
 
+        Button selectFile = view.findViewById(R.id.selectFile);
+        notif = view.findViewById(R.id.notif);
+
+        selectFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getActivity(),"clicked", Toast.LENGTH_SHORT).show();
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED)
+                {
+                    //Toast.makeText(getActivity(),"in if", Toast.LENGTH_SHORT).show();
+                    selectPdf();
+                }
+                else
+                {
+                    //Toast.makeText(getActivity(),"else", Toast.LENGTH_SHORT).show();
+                    ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},9);
+                }
+
+            }
+        });
+
 
         button.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -114,7 +145,7 @@ public class ApplicationFragment extends Fragment {
 
                 MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getContext());
                 builder.setTitle("Confirm Submission");
-                builder.setMessage("Please confirm details:\n\nSubject Name: " + name +"\nSubject Code: "+ code + "\nCurrent Grade: "+ grade);
+                builder.setMessage("Please confirm details:\n\nSubject Name: " + name +"\nSubject Code: "+ code + "\nCurrent Grade: "+ grade + "\nFile Chosen: "+ pdfUri.toString());
                 builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
@@ -152,17 +183,43 @@ public class ApplicationFragment extends Fragment {
                     }
                 });
                 builder.show();
-
-
-
-
-
-
             }
         });
 
         return view;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 9 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+        {
+            selectPdf();
+        }
+        else
+            Toast.makeText(getActivity(),"Please provide Permission", Toast.LENGTH_SHORT).show();
+    }
 
+    private void selectPdf()
+    {
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 13);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode == 13 && resultCode == RESULT_OK && data!= null)
+        {
+            pdfUri = data.getData();
+            notif.setText(pdfUri.toString());
+
+        }
+        else
+        {
+            Toast.makeText(getActivity(),"Please select file", Toast.LENGTH_SHORT).show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
